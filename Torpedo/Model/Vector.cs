645a525ebf1d5e9b9b513a7001ff63a5894fbbbd
polgar janos <polgarjano@gmail.com>
2022-01-03 21:43:02 +0100

    Adding Vector

---
+++ Torpedo/Model/Vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torpedo.Model
+{
+    struct Vector
+    {
+        public static readonly Vector Up = new Vector(0, -1);
+        public static readonly Vector Down = new Vector(0, 1);
+        public static readonly Vector Right = new Vector(1, 0);
+        public static readonly Vector Left = new Vector(-1, 0);
+
+        public int Y { get; set; }
+
+        public int X { get; set; }
+
+
+        public Vector(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static bool operator ==(Vector a, Vector b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+
+        public static bool operator !=(Vector a, Vector b)
+        {
+            return !(a == b);
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X + b.X, a.Y + b.Y);
+        }
+    }
+}
+
