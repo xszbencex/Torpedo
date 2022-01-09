907a272ac4af0b13dd51f74640e87511f3facff6
Szabó Bence <xszbencex@gmail.com>
2022-01-09 12:10:28 +0100

    Colors to MainSettings and Ship hover basics

--- Torpedo/Settings/MainSettings.cs
+++ Torpedo/Settings/MainSettings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using Torpedo.Model;
 
 namespace Torpedo.Settings
@@ -11,6 +12,11 @@ namespace Torpedo.Settings
     {
         public const int GridHeight = 10, GridWidth = 10;
         public static readonly int[] PlayableShipsLength = new int[] { 2, 2 };
+        public static readonly Brush DefaultFieldColor = Brushes.Transparent;
+        public static readonly Brush DefaultFieldStrokeColor = Brushes.Black;
+        public static readonly Brush ShipColor = Brushes.Blue;
+        public static readonly Brush HitColor = Brushes.Coral;
+        public static readonly Brush MissColor = Brushes.DarkGray;
 
         public static bool CoordinateValidation(Vector coordinate)
         {
