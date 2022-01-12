9bced64f11ba8e175ef16984d826252682dd600c
szatmariboldizsar <szatmariboldizsar@gmail.com>
2022-01-12 15:49:02 +0100

    EntityFW base for Match history created

---
+++ Torpedo/Model/Match.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Torpedo.Model
+{
+    public class Match
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+        public int Rounds { get; set; }
+        public int Player1Hits { get; set; }
+        public int Player2Hits { get; set; }
+        public string Winner { get; set; }
+
+        public Match(string player1Name, string player2Name, int rounds, int player1Hits, int player2Hits, string winner)
+        {
+            this.Player2Name = player2Name;
+            this.Rounds = rounds;
+            this.Player1Hits = player1Hits;
+            this.Player2Hits = player2Hits;
+            this.Winner = winner;
+        }
+    }
+}
---
+++ Torpedo/Repository/MatchRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torpedo.Model;
+
+namespace Torpedo.Repository
+{
+    internal class MatchRepository
+    {
+        public static IList<Match> GetMatches()
+        {
+            using (var database = new MatchContext())
+            {
+                var matches = database.Matches.ToList();
+
+                return matches;
+            }
+        }
+
+        public static Match GetMatch(long id)
+        {
+            using (var database = new MatchContext())
+            {
+                var match = database.Matches.Where(m => m.Id == id).FirstOrDefault();
+
+                return match;
+            }
+        }
+
+        public static void AddMatch(Match match)
+        {
+            using (var database = new MatchContext())
+            {
+                database.Matches.Add(match);
+
+                database.SaveChanges();
+            }
+        }
+    }
+}
