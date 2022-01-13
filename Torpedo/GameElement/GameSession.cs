cad1bec8b95c5d4fdc5b29cf65f754dfd040bca3
szatmariboldizsar <szatmariboldizsar@gmail.com>
2022-01-13 17:56:37 +0100

    Code cleanup

--- Torpedo/GameElement/GameSession.cs
+++ Torpedo/GameElement/GameSession.cs
@@ -16,7 +16,6 @@ namespace Torpedo.GameElement
         public bool IsPuttingDownPhase { get; set; }
         public Vector? ShipStartPoint { get; set; }
         public bool GameOver { get; set; }
-        public int Winner { get; set; }
         public int RoundNumber { get; set; }
         public bool RoundSwitch { get; set; }
         public GameSession(Player player1, Player player2)
@@ -28,32 +27,15 @@ namespace Torpedo.GameElement
             this.RoundNumber = 1;
             this.RoundSwitch = false;
         }
-        /// <summary>
-        /// !!!!Nincs tesztelve hogy a kordináták a táblán vannak e és egy vonalban vannak e
-        /// !!!Nincs tesztelve a halyó hosz se
-        /// 
-        /// </summary>
-        /// <param name="shipStartPoint"></param>
-        /// <param name="shipEndPoint"></param>
+
         public void ActualPlayerPutsDownShip(Vector shipStartPoint, Vector shipEndPoint)
         {
-            // TODO actual player PutDownShip-jét meghívni,
-            // ha lerakta az összeset, akkor váltani az actual playert a másikra
-            // ha a másik is lerakta az összeset vagy ha az enemy AI akkor hívni a PutAllDown-t és
-            // IsPuttingDownPhase = false-ra állítása
-
-            // Ha az ai nál a PutDownShip-re lerakja az összeset és a shipCout ott max ra állitja (ShipCountott most adtam hozzá)
-            // akkor nincs szükség a PutDownAllShipre  Anyi hogy a paraméterek lesznek figyelmen kivűl hagyva azt nem tudom menyire gáz???
-
              ActualPlayer.PutDownAShip(shipStartPoint, shipEndPoint);
-            // player2 ai??
 
             if ((Player1.ShipCount == MainSettings.PlayableShipsLength.Length) && (Player2.ShipCount == MainSettings.PlayableShipsLength.Length))
             {
                 IsPuttingDownPhase = false;
                 SecondPhaseInIt();
-                // eldönteni hogy ki kezd
-                // lőni ha ai kezd
             }
 
             if (ActualPlayer.ShipCount == MainSettings.PlayableShipsLength.Length)
@@ -82,11 +64,6 @@ namespace Torpedo.GameElement
 
         public void ActualPlayerTakeAShot(Vector shotPoint)
         {
-            // TODO shotPoint kezelése, azzal kell hívni az actual player TakeAShot-ját
-            // A TakeShot azt Bemenetnek Szántam volna de ha innen meg kapja akkor igazábol lehet hogy felesleges is
-            // Ennek a végén lehet meg lehetne már hívni a GameOvert mert egy lövés után lesz vége meg meg lehet álapitani hogy kinyert az aki épen lőtt
-            // és ha arra fel lehet iratkozni akkor innen ji lehet váltani a játék végét (??? nem vagyok benne teljesen biztos hogy ez működhet)
-
             RegisteringAShot(shotPoint);
 
             ActualPlayer = GetOtherPlayer();
@@ -94,9 +71,6 @@ namespace Torpedo.GameElement
             {
                 AIShot();
             }
-
-            // Ai elleni játéknál ha nem nyert az igazi játékos akkor it lőhet az ai és meg
-            // vizsgáljuk, hogy az ai nyert e majd vissza álitjuk az actualPlayert nem tünik szép megoldásnak de működhet
         }
 
         private void AIShot()
